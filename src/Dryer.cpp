c8a8d5a4522fc0debc624658e36a3670c53692ec
Tobias Koppmann <tobias.koppmann@kannegiesser.de>
2023-04-04 17:36:24 +0200

    refactoring

--- src/Dryer.cpp
+++ src/Dryer.cpp
@@ -1,130 +1,136 @@
 #include <Arduino.h>
 #include "Dryer.h"
 
-const int MOTOR_PIN_1 = 13;
-const int MOTOR_PIN_2 = 12;
-const int MOTOR_PIN_3 = 14;
-const int MOTOR_PIN_4 = 27;
+#define MOTOR_PIN_1 13
+#define MOTOR_PIN_2 12
+#define MOTOR_PIN_3 14
+#define MOTOR_PIN_4 27
 
-const unsigned long timeout_run_motor = 5000;
-const unsigned long timeout_stop_motor = 1000;
-const unsigned long timeout_step_ramp = 500;
+#define TIME_RUN_MOTOR 5000
+#define TIME_STOP_MOTOR 1000
+#define TIME_STEP_RAMP 500
+#define MAX_RAMP_COUNT 8
+#define MIN_RAMP_COUNT 2
+
 unsigned int step_motor = 0;  
 unsigned long step_start_millis = 0;
 uint8_t ramp_counter = 0;
 unsigned int ramp_step_time = 0;
 
 void Motorsetup() {
-  pinMode(MOTOR_PIN_1, OUTPUT);
-  pinMode(MOTOR_PIN_2, OUTPUT);
-  pinMode(MOTOR_PIN_3, OUTPUT);
-  pinMode(MOTOR_PIN_4, OUTPUT);
+    pinMode(MOTOR_PIN_1, OUTPUT);
+    pinMode(MOTOR_PIN_2, OUTPUT);
+    pinMode(MOTOR_PIN_3, OUTPUT);
+    pinMode(MOTOR_PIN_4, OUTPUT);
 }
 
 void Motorloop(){
     switch(step_motor)
-  {
-    case 0:
-      step_start_millis = millis();
-      ramp_step_time = millis();
-      ramp_counter = 2;
-      step_motor = 1;
-      break;
+    {
+        case 0:
+            step_start_millis = millis();
+            ramp_step_time = millis();
+            ramp_counter = 2;
+            step_motor = 1;
+        break;
 
-    case 1:
-      if (millis() > ramp_step_time + timeout_step_ramp)
-      {
-        ramp_step_time = millis();
-        ramp_counter++;
-      }
-      
-      analogWrite(MOTOR_PIN_1, min(255, 32*ramp_counter-1));
-      
-      if(ramp_counter >= 8) {
-        analogWrite(MOTOR_PIN_1, 255);
-        ramp_counter = 0;
-        step_start_millis = millis();
-        step_motor = 2;
-      }
-      break;
+        case 1:
+            if (millis() > ramp_step_time + TIME_STEP_RAMP)
+            {
+              ramp_step_time = millis();
+              ramp_counter++;
+            }
+            
+            analogWrite(MOTOR_PIN_1, min(255, 32*ramp_counter-1));
+            
+            if(ramp_counter >= MAX_RAMP_COUNT)
+            {
+              analogWrite(MOTOR_PIN_1, 255);
+              ramp_counter = 0;
+              step_start_millis = millis();
+              step_motor = 2;
+            }
+        break;
 
-    case 2:
-      if (millis() > step_start_millis + timeout_run_motor)
-      {
-        step_start_millis = millis();
-        ramp_step_time = millis();
-        ramp_counter = 8;
-        step_motor = 3;
-      }
-      break;
+        case 2:
+            if (millis() > step_start_millis + TIME_RUN_MOTOR)
+            {
+              step_start_millis = millis();
+              ramp_step_time = millis();
+              ramp_counter = MAX_RAMP_COUNT;
+              step_motor = 3;
+            }
+        break;
 
-    case 3:
-      if (millis() > ramp_step_time + timeout_step_ramp)
-      {
-        ramp_step_time = millis();
-        ramp_counter--;
-      }
-      
-      analogWrite(MOTOR_PIN_1, min(255, 32*ramp_counter-1));
-      
-      if(ramp_counter < 2) {
-        analogWrite(MOTOR_PIN_1, 0);
-        ramp_counter = 0;
-        step_start_millis = millis();
-        step_motor = 4;
-      }
-      break;
+        case 3:
+            if (millis() > ramp_step_time + TIME_STEP_RAMP)
+            {
+                ramp_step_time = millis();
+                ramp_counter--;
+            }
+            
+            analogWrite(MOTOR_PIN_1, min(255, 32*ramp_counter-1));
+            
+            if(ramp_counter < MIN_RAMP_COUNT)
+            {
+                analogWrite(MOTOR_PIN_1, 0);
+                ramp_counter = 0;
+                step_start_millis = millis();
+                step_motor = 4;
+            }
+        break;
 
-    case 4:
-      step_start_millis = millis();
-      ramp_step_time = millis();
-      ramp_counter = 2;
-      step_motor = 5;
-      break;
+        case 4:
+            step_start_millis = millis();
+            ramp_step_time = millis();
+            ramp_counter = 2;
+            step_motor = 5;
+        break;
 
-    case 5:
-      if (millis() > ramp_step_time + timeout_step_ramp)
-      {
-        ramp_step_time = millis();
-        ramp_counter++;
-      }
-      
-      analogWrite(MOTOR_PIN_2, min(255, 32*ramp_counter-1));
-      
-      if(ramp_counter >= 8) {
-        analogWrite(MOTOR_PIN_2, 255);
-        ramp_counter = 0;
-        step_start_millis = millis();
-        step_motor = 6;
-      }
-      break;
+        case 5:
+            if (millis() > ramp_step_time + TIME_STEP_RAMP)
+            {
+                ramp_step_time = millis();
+                ramp_counter++;
+            }
+            
+            analogWrite(MOTOR_PIN_2, min(255, 32*ramp_counter-1));
+            
+            if(ramp_counter >= MAX_RAMP_COUNT)
+            {
+                analogWrite(MOTOR_PIN_2, 255);
+                ramp_counter = 0;
+                step_start_millis = millis();
+                step_motor = 6;
+            }
+        break;
 
-    case 6:
-      if (millis() > step_start_millis + timeout_run_motor)
-      {
-        step_start_millis = millis();
-        ramp_step_time = millis();
-        ramp_counter = 8;
-        step_motor = 7;
-      }
-      break;
+        case 6:
+            if (millis() > step_start_millis + TIME_RUN_MOTOR)
+            {
+                step_start_millis = millis();
+                ramp_step_time = millis();
+                ramp_counter = MAX_RAMP_COUNT;
+                step_motor = 7;
+            }
+        break;
 
-    case 7:
-      if (millis() > ramp_step_time + timeout_step_ramp)
-      {
-        ramp_step_time = millis();
-        ramp_counter--;
-      }
+        case 7:
+            if (millis() > ramp_step_time + TIME_STEP_RAMP)
+            {
+                ramp_step_time = millis();
+                ramp_counter--;
+            }
       
-      analogWrite(MOTOR_PIN_2, min(255, 32*ramp_counter-1));
-      
-      if(ramp_counter < 2) {
-        analogWrite(MOTOR_PIN_2, 0);
-        ramp_counter = 0;
-        step_start_millis = millis();
-        step_motor = 0;
-      }
-      break;
-
+            analogWrite(MOTOR_PIN_2, min(255, 32*ramp_counter-1));
+          
+            if(ramp_counter < MIN_RAMP_COUNT) 
+            {
+                analogWrite(MOTOR_PIN_2, 0);
+                ramp_counter = 0;
+                step_start_millis = millis();
+                step_motor = 0;
+            }
+        break;
   }
 }
--- src/LED.cpp
+++ src/LED.cpp
@@ -1,94 +1,87 @@
+// externe Libraries 
 #include <NeoPixelBus.h>
 #include <NeoPixelBrightnessBus.h>
+ 
 #include "LED.h"
 
-const int LED_DATA_PIN = 26;
+#define LED_DATA_PIN 26
+#define COLOR_SATURATION 255  // set color to 128, this can be between 0 (black, regardless of hue or saturation) and 255 (maximum brightness).
+#define PIXEL_COUNT 7 // count of LEDs in the strip
+const unsigned long timeout_led_brighten= 50; //time for each ledbrightenstadium
+unsigned int step_led = 0; // steps in the ledbrightenprozess, first it must set to step 0, because we start counting with 0 not with 1 
+unsigned long step_led_start_millis = 0; // ms have set to 0, start timer at time 0
 
-const uint16_t PixelCount = 7; // this example assumes 4 pixels, making it smaller will cause a failure
-const uint8_t PixelPin = 26;  // make sure to set this to the correct pin, ignored for Esp8266
+NeoPixelBus<NeoGrbFeature, NeoWs2811Method> strip(PIXEL_COUNT, LED_DATA_PIN);
 
-#define colorSaturation 128
-
-const unsigned long timeout_led_brighten= 50;
-const unsigned long timeout_led_darken = 1000;
-unsigned int step_led = 0;
-unsigned long step_led_start_millis = 0;
-uint8_t ramp_led_counter = 0;
-unsigned int ramp_led_step_time = 0;
-
-NeoPixelBus<NeoGrbFeature, NeoWs2811Method> strip(PixelCount, PixelPin);
-
-const uint8_t c_MinBrightness = 8; 
-const uint8_t c_MaxBrightness = 96;
+const uint8_t c_MinBrightness = 8; // min Brightness for LED Loop is 8, value can be between 0 and 255
+const uint8_t c_MaxBrightness = 96; // max Brightness for LED Loop is 96,  value can be between 0 and 255  
 int8_t direction = -1;
 uint8_t brightness = c_MaxBrightness;
 
-const RgbColor red(colorSaturation, 0, 0);
-const RgbColor green(0, colorSaturation, 0);
-const RgbColor blue(0, 0, colorSaturation);
+// definitions of the colors red, green, blue and black 
+const RgbColor red(COLOR_SATURATION, 0, 0);
+const RgbColor green(0, COLOR_SATURATION, 0);
+const RgbColor blue(0, 0, COLOR_SATURATION);
 const RgbColor black(0);
 
 // setup 
-void ledsetup() {
-  pinMode(LED_DATA_PIN, OUTPUT);
+void ledsetup()
+{
+    // set PIN to Output
+    pinMode(LED_DATA_PIN, OUTPUT);
 
-  // this resets all the neopixels to an off state
-  strip.Begin();
-  strip.Show();
-
-  for (size_t i = 0; i < PixelCount; i++)
-  {
-    strip.SetPixelColor(i, RgbColor(0,0,brightness));
-  }
-
-  strip.Show();
-
-  Serial.println();
-  Serial.println("Running...");
+    // this resets all the neopixels to an off state
+    strip.Begin();
+    strip.Show();
 }
 
 // loop 
-void led_loop() {
-  
-  // ### LED LOOP ###
-  switch (step_led) {
-
-    case 0:
-      
-      if (brightness <= c_MinBrightness)
+void led_loop()
+{
+    // ### LED LOOP ###
+    switch (step_led)
     {
-      direction = 1;
+        case 0:
+            // If the brightness is lower than the min brightness, it should be increased again by 1
+            if (brightness <= c_MinBrightness)
+            {
+                direction = 1;
+            }
+            // If the brightness is greater than the max. brightness, it should be reduced by 1 again.
+            else if (brightness >= c_MaxBrightness)
+            {
+                direction = -1;
+            }
+
+            // apply dimming
+            brightness += direction;
+
+            for (size_t i = 0; i < PIXEL_COUNT; i++)
+            {
+                strip.SetPixelColor(i, RgbColor(0,0,brightness));
+            }
+
+            // show the results
+            strip.Show();
+            step_led_start_millis = millis();
+            step_led = 1;
+        break; 
+
+        case 1:
+          // after 50 ms the prozess will start again - it will beginn with case 0
+          if (millis() > step_led_start_millis + timeout_led_brighten)
+          {
+              step_led = 0;
+          }
+        break;      
     }
-    else if (brightness >= c_MaxBrightness)
-    {
-      direction = -1;
-    }
-    // apply dimming
-    brightness += direction;
-
-    for (size_t i = 0; i < PixelCount; i++)
-    {
-      strip.SetPixelColor(i, RgbColor(0,0,brightness));
-    }
-
-    // show the results
-    strip.Show();
-
-      step_led_start_millis = millis();
-      step_led = 1;
-      break;
-
-    case 1:
-      if (millis() > step_led_start_millis + timeout_led_brighten)
-      {
-        step_led = 0;
-      }
-      break;
-      
-  }
 }
 
-  void led_off(){
-    strip.SetPixelColor(0, RgbColor(0));
-  }
+// ### LED off ### 
+void led_off()
+{
+  // Set LEDPixelColor to black
+  strip.ClearTo(black);
+  strip.Show();
+}
     
